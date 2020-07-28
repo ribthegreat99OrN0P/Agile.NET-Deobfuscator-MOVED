241517de1a1837155f3d93f010a7cf4cac243162
ribthegreat99OrN0P <52993096+ribthegreat99OrN0P@users.noreply.github.com>
2020-07-28 10:48:57 +1000

    Proxy delegates fixed
    I found the problem and fixed it! Working 100% now

--- Agile.NET-Deobfuscator-DNLIB/Protections/ProxyDelegatesModule.cs
+++ Agile.NET-Deobfuscator-DNLIB/Protections/ProxyDelegatesModule.cs
@@ -13,7 +13,7 @@ namespace Agile.NET_Deobfuscator_DNLIB.Protections
     public class ProxyDelegatesModule
     {
 
-        private static FieldDef testfield = null;
+        private static bool flag = false;
         private static int removed = 0;
         public static void Execute()
         {
@@ -27,47 +27,27 @@ namespace Agile.NET_Deobfuscator_DNLIB.Protections
                         {
                             for (int i = 0; i < method.Body.Instructions.Count; i++)//Go through all the instructions in the method
                             {
-                                //Here we find the(Ldsfld)opcode which has the delegate field operand and we set it to out local variable
-                                if (method.Body.Instructions[i].OpCode == dnlib.DotNet.Emit.OpCodes.Ldsfld)
+                                if (method.Body.Instructions[i].OpCode == dnlib.DotNet.Emit.OpCodes.Call &&
+                               method.Body.Instructions[i].Operand.ToString().Contains("::Invoke"))
                                 {
-                                    object operand = method.Body.Instructions[i].Operand;
-                                    
-                                    if(operand is FieldDef field)
+
+                                    var op = method.Body.Instructions[i].Operand;
+                                    if (op is MethodDef mm)
                                     {
-                                        testfield = field;
-                                        method.Body.Instructions[i].OpCode = dnlib.DotNet.Emit.OpCodes.Nop;//Replace it with Nop because we dont need it anymore
+                                        MemberRef solved = gettt(mm);
+
+                                        method.Body.Instructions[i].Operand = solved;
+                                        removed++;
                                     }
                                 }
-                                //here we find then the Invoke method which executes the process of the delegate and we replace the operand with the actual method
-                                else if(method.Body.Instructions[i].OpCode == dnlib.DotNet.Emit.OpCodes.Call &&
-                                    method.Body.Instructions[i].Operand.ToString().Contains("::Invoke"))
-                                {
-                                    method.Body.Instructions[i].Operand = ResolveDelegate(testfield);
-                                    removed++;
-                                }
-                                
-                                /*
                                 if (method.Body.Instructions[i].OpCode == dnlib.DotNet.Emit.OpCodes.Ldsfld)
                                 {
-                                    object operand = method.Body.Instructions[i].Operand;
-
-                                    if (operand is FieldDef field)
+                                    var op2 = method.Body.Instructions[i].Operand;
+                                    if (op2 is FieldDef ff && ff.DeclaringType.BaseType.FullName == "System.MulticastDelegate")
                                     {
-                                        testfield = field;
-
                                         method.Body.Instructions[i].OpCode = dnlib.DotNet.Emit.OpCodes.Nop;
                                     }
-                                    for (int p = i; p < method.Body.Instructions.Count; p++)
-                                    {
-                                        if(method.Body.Instructions[p].OpCode == dnlib.DotNet.Emit.OpCodes.Call
-                                            && method.Body.Instructions[p].Operand.ToString().Contains("::Invoke"))
-                                        {
-                                            method.Body.Instructions[p].Operand = ResolveDelegate(testfield);
-                                            removed++;
-                                        }
-                                    }
                                 }
-                                */
                             }
                         }
                     }
@@ -77,41 +57,47 @@ namespace Agile.NET_Deobfuscator_DNLIB.Protections
             Console.WriteLine(string.Format("[+] Fixed {0} delegates", removed));
         }
 
-        private static MemberRef ResolveDelegate(FieldDef field)//Most of this process was copied from the method from Agile
+        private static MemberRef gettt(MethodDef met)
         {
-            MemberRef result = null;
-            TypeDef deleType = field.DeclaringType;//Get the type of the field
-            if (deleType.BaseType.FullName == "System.MulticastDelegate")//Check to see if the type is a delegate
+            TypeDef decType = met.DeclaringType;
+            if (decType.BaseType.FullName == "System.MulticastDelegate")
             {
-                foreach (MethodDef meth in deleType.Methods)//Go through all the methods in the type
+                foreach (MethodDef method in decType.Methods)
                 {
-                    if (meth.HasBody && meth.Body.HasInstructions)//Check to see if the method has a valid body with instructions
+                    if (method.HasBody && method.Body.HasInstructions)
                     {
-                        for (int o = 0; o < meth.Body.Instructions.Count; o++)//Go through all the instructions in the method
+                        for (int i = 0; i < method.Body.Instructions.Count; i++)
                         {
-                            if (meth.Body.Instructions[o].OpCode == dnlib.DotNet.Emit.OpCodes.Ldc_I4)//find the int to use when solving the method
+                            if (method.Body.Instructions[i].IsLdcI4())
                             {
-                                int valuetest = meth.Body.Instructions[o].GetLdcI4Value();//Get the value
-                                string name = field.Name;//Get the field name
-                                bool flag = false;
-                                if (name.EndsWith("%"))//This method is originally from Agile
+
+                                int value = method.Body.Instructions[i].GetLdcI4Value();
+                                FieldDef fieldDele = getfieldofdelegate(decType);
+
+                                string name = fieldDele.Name;
+                                if (name.EndsWith("%"))
                                 {
                                     flag = true;
-                                    name = name.TrimEnd(new char[]
-                                    {
-                                       '%'
-                                    });
+                                    name = name.TrimEnd(new char[] { '%' });
                                 }
                                 uint num = BitConverter.ToUInt32(Convert.FromBase64String(name), 0);
                                 ModuleDefMD MDMod = (ModuleDefMD)Globals.ASM.ManifestModule;
-                                MemberRef solvedMemberRef = MDMod.ResolveMemberRef((uint)((long)num + 167772161L) - 167772160U);//Resolve the method!
-                                result = solvedMemberRef;//set the local variable with the resolved memberef
+                                MemberRef solvedMemberRef = MDMod.ResolveMemberRef((uint)((long)num + 167772161L) - 167772160U);
+                                return solvedMemberRef;
                             }
                         }
                     }
                 }
             }
-            return result;//return the result
+            return null;
+        }
+        private static FieldDef getfieldofdelegate(TypeDef type)
+        {
+            foreach (var item in type.Fields)
+            {
+                return item;
+            }
+            return null;
         }
 
 
